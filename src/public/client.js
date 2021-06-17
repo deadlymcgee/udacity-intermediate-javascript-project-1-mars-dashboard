3bb0896552dbb8f86896504e1a2cca5f6e4e2cd5
npwaters <40352015+npwaters@users.noreply.github.com>
2021-06-17 16:35:47 +1000

    fix: Increase click target area for menu items

--- src/public/client.js
+++ src/public/client.js
@@ -49,8 +49,8 @@ window.addEventListener('load', () => {
 // ------------------------------------------------------  COMPONENTS
 const Rover = (rover) => {
     return `
-        <div class="rover-menu-item">
-            <h4 onclick="getRoverDetails('${rover}')">${rover}</h4>
+        <div class="rover-menu-item" onclick="getRoverDetails('${rover}')">
+            <h4>${rover}</h4>
         </div>
     `
 }
